9fd545003dc8baa8e8a289e531340956f5471782
Nathan Pedigo <njpedigo@gmail.com>
2023-02-03 12:01:57 -0700

    finished styling need to finish the js

---
+++ app.js
@@ -0,0 +1,10 @@
+// get the container element
+    // by id
+// get the grid elements
+    // by id
+// player1 is green
+    // background colorChange function
+// player2 is purple
+    // background colorChange function
+// change between players
+
